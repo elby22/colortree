591e53ea3a0a753789751c42dfd3059447d1c8f8
Elby Basolis <elby223@gmail.com>
2020-02-04 23:26:14 -0500

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app" class="tree">
+		<node :root="root"></node>
+
+		<div class="landing" v-if="!hasChildren">
+			<div class="words">Click to split</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Node from './components/Node';
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	components: {
+		Node
+	},
+	data(){
+		return {
+			root: {
+				color: 'blue',
+				width: 100,
+				children: []
+			}
+		}
+	},
+	computed: {
+		hasChildren(){
+			return this.root.children.length;
+		}
+	},
+	methods: {
+	}  
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	*{
+		box-sizing: border-box;
+	}
+	body{
+		margin: 0px;
+	}
+	#app{
+		height: 100vh;
+		width: 100vw;
+		position: relative;
+	}
+	.landing{
+		top: 0px;
+		position: absolute;
+		height: 100vh;
+		width: 100vw;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		pointer-events: none;
+	}
 </style>
